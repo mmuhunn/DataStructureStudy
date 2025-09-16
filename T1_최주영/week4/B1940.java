b010fc38291869bf8aa4f34350f7413e4d9e8d4d
ichbinju0 <hahalove79@naver.com>
2025-09-16 22:20:44 +0900

    문제풀고 있지만 틀림

--- T1_최주영/week4/B1940.java
+++ T1_최주영/week4/B1940.java
@@ -5,30 +5,41 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class B1940 {
-    public int solution(int n, int m, int[] ints){
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < ints.length; i++) {
-            arrayList.add(ints[i]);
+        public int solution(int n, int m, int[] ints) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int i = 0; i < ints.length; i++) {
+                arrayList.add(ints[i]);
+            }
+            Collections.sort(arrayList);
+            int pointer1 = 0;
+            int pointer2 = ints.length;
+            int sum = 0;
+            int count = 0;
+            while (pointer1 < pointer2){
+                sum = pointer1 + pointer2;
+                if (sum == m){
+                    count++;
+                    pointer1++;
+                    pointer2--;
+                }else if(sum < m){
+                    pointer1--;
+                }else{
+                    pointer2++;
+                }
+            }
+            return count;
+
         }
-        Collections.sort(arrayList);
-        int pointer1 = 0;
-        int pointer2 = ints.length;
-        int sum = 0;
-        while (sum == ints)
 
-
-    }
-
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        B1940 main = new B1940();
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] ints = new int[n];
-        for (int i = 0; i < n; i++) {
-            ints[i] = scanner.nextInt();
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            B1940 main = new B1940();
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int[] ints = new int[n];
+            for (int i = 0; i < n; i++) {
+                ints[i] = scanner.nextInt();
+            }
+            System.out.println(main.solution(n,m,ints));
         }
-        System.out.println(main.solution(n,m,ints));
     }
-}
