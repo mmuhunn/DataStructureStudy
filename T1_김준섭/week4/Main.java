5b62b529ba4628f6587e7e9706e6dc5e1622f386
Junseop Kim <junseopkim1228@gmail.com>
2025-09-22 22:28:14 +0900

    reset

---
+++ T1_김준섭/week4/Main.java
@@ -0,0 +1,23 @@
+// 백준 1940번 문제: 주몽 실버 4
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int M = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        
+
+    }
+}
